687b33211c6d8d5645cf5ebcb8383bfaee5773d6
KyleT_Hoffman <kyletroyhoffman@gmail.com>
2025-03-18 22:08:03 -0400

    Done

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+
+setup(
+
+    name='Demo',
+
+    version='0.1',
+
+    description='A simple demonstration package',
+
+    url='http://github.com/ontoligent/m09-demo',
+
+    author='Kyle Hoffman',
+
+    author_email='zuc7ze@virginia.edu',
+
+    license='MIT',
+
+    packages=['demo']
+
+)
